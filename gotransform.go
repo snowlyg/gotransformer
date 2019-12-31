2ce715af015a1f17813fb1638a00ea7a7dcc975d
luodan@elinkport.com <linximeng>
2019-12-31 16:27:02 +0800

    增加 map 数据转换

--- gotransform.go
+++ gotransform.go
@@ -28,50 +28,72 @@ func NewTransform(outObj, inObj interface{}, timeFormat string) *Transform {
 	}
 }
 
+// 输出数据的值
 func (t *Transform) GetOutputValue() reflect.Value {
 	return reflect.ValueOf(t.OutputObj)
 }
 
+// 输出数据的值类型
 func (t *Transform) GetOutputValueKind() reflect.Kind {
 	return t.GetOutputValue().Kind()
 }
 
+// 输出数据的值的成员
 func (t *Transform) GetOutputValueElem() reflect.Value {
 	return reflect.ValueOf(t.OutputObj).Elem()
 }
 
+// 输出数据的值的成员类型
 func (t *Transform) GetOutputValueElemType() reflect.Type {
 	return reflect.ValueOf(t.OutputObj).Elem().Type()
 }
 
+// 输出数据的值的成员的值
 func (t *Transform) GetOutputValueElemField(i int) reflect.Value {
 	return reflect.ValueOf(t.OutputObj).Elem().Field(i)
 }
 
+// 输出数据的值的成员类型的值
 func (t *Transform) GetOutputValueElemTypeField(i int) reflect.StructField {
 	return reflect.ValueOf(t.OutputObj).Elem().Type().Field(i)
 }
 
+// 输入数据的值
 func (t *Transform) GetInsertValue() reflect.Value {
 	return reflect.ValueOf(t.InsertObj)
 }
 
+// 输入 map类型数据的 keys
+func (t *Transform) GetInsertMapKeys() []reflect.Value {
+	return reflect.ValueOf(t.InsertObj).MapKeys()
+}
+
+// 输入map类型数据的 key 对应数数据
+func (t *Transform) GetInsertMapValue(key reflect.Value) reflect.Value {
+	return reflect.ValueOf(t.InsertObj).MapIndex(key)
+}
+
+// 输入数据的值类型
 func (t *Transform) GetInsertValueKind() reflect.Kind {
 	return t.GetInsertValue().Kind()
 }
 
+// 输入数据的值的成员
 func (t *Transform) GetInsertValueElem() reflect.Value {
 	return reflect.ValueOf(t.InsertObj).Elem()
 }
 
+// 输入数据的值的成员的类型
 func (t *Transform) GetInsertValueElemType() reflect.Type {
 	return reflect.ValueOf(t.InsertObj).Elem().Type()
 }
 
+// 输入数据的值的成员的值
 func (t *Transform) GetInsertValueElemField(i int) reflect.Value {
 	return reflect.ValueOf(t.InsertObj).Elem().Field(i)
 }
 
+// 输入数据的值的成员类型的值
 func (t *Transform) GetInsertValueElemTypeField(i int) reflect.StructField {
 	return reflect.ValueOf(t.InsertObj).Elem().Type().Field(i)
 }
@@ -82,26 +104,33 @@ func (t *Transform) Transformer() error {
 		return errors.New("输出数据格式必须是指针")
 	}
 
+	if t.GetInsertValueKind() == reflect.Map {
+		t.transformerMap()
+	} else if t.GetInsertValueKind() == reflect.Ptr {
+		t.transformerPtr()
+	}
+
+	return nil
+}
+
+// ptr 类型数据转换
+func (t *Transform) transformerPtr() {
 	for i := 0; i < t.GetOutputValueElem().NumField(); i++ {
 		of := t.GetOutputValueElemField(i)
 		otf := t.GetOutputValueElemTypeField(i)
-		// fmt.Printf("OutputType =》 %v , OutputValue => %v", otf, of)
 		if !t.GetOutputValueElem().CanSet() {
 			fmt.Printf("OutputType =》 %v , OutputValue => %v  \n", otf, of)
 			continue
 		}
-
 		tag := t.getTag(otf)
 		timeFormat := ""
 		for iI := 0; iI < t.GetInsertValueElem().NumField(); iI++ {
 			inf := t.GetInsertValueElemField(iI)
 			into := t.GetInsertValueElemTypeField(iI)
 			if tag != nil {
-
 				if tag.Key == "Time" {
 					timeFormat = tag.Value
 				}
-
 				var args []reflect.Value
 				startFunc := false
 				// 执行自定义方法
@@ -115,7 +144,6 @@ func (t *Transform) Transformer() error {
 						args = append(args, reflect.ValueOf(vt))
 					}
 				}
-
 				if tag.Key == "Func" && startFunc {
 					rs := t.CallOutFunc(tag).Call(args)
 					if rs[0].Interface() != nil {
@@ -132,7 +160,6 @@ func (t *Transform) Transformer() error {
 					}
 				}
 			}
-
 			if into.Name == otf.Name {
 				if into.Type.Name() == "Time" {
 					of.SetString(t.setTime(inf, "", timeFormat))
@@ -144,7 +171,6 @@ func (t *Transform) Transformer() error {
 					continue
 				}
 			}
-
 			if into.Name == "BaseModel" {
 				if otf.Name == "Id" {
 					of.SetInt(inf.FieldByName("Id").Interface().(int64))
@@ -160,8 +186,90 @@ func (t *Transform) Transformer() error {
 
 		}
 	}
+}
 
-	return nil
+// map 类型数据转换
+func (t *Transform) transformerMap() {
+	for i := 0; i < t.GetOutputValueElem().NumField(); i++ {
+		of := t.GetOutputValueElemField(i)
+		otf := t.GetOutputValueElemTypeField(i)
+		if !t.GetOutputValueElem().CanSet() {
+			fmt.Printf("OutputType =》 %v , OutputValue => %v  \n", otf, of)
+			continue
+		}
+		tag := t.getTag(otf)
+		timeFormat := ""
+
+		for _, k := range t.GetInsertMapKeys() {
+			inf := t.GetInsertMapValue(k)
+			keyName := k.String()
+			if !of.CanSet() {
+				fmt.Printf("keyName:不能被修改 %v \n", keyName)
+				continue
+			}
+
+			if tag != nil {
+				if tag.Key == "Time" {
+					timeFormat = tag.Value
+				}
+				var args []reflect.Value
+				startFunc := false
+				// 执行自定义方法
+				if len(tag.FiledName) < 1 { // 标签只定义了一个参数，则默认第一个参数为 inf.String()
+					args = []reflect.Value{reflect.ValueOf(inf.String())} // append args,first arg is inf.string()
+					startFunc = keyName == otf.Name
+				} else {
+					startFunc = keyName == tag.FiledName
+					args = append(args, t.GetInsertValueElem().FieldByName(tag.FiledName))
+					for _, vt := range tag.Args {
+						args = append(args, reflect.ValueOf(vt))
+					}
+				}
+				if tag.Key == "Func" && startFunc {
+					rs := t.CallOutFunc(tag).Call(args)
+					if rs[0].Interface() != nil {
+
+						of.SetString(rs[0].Interface().(string))
+						continue
+					}
+				}
+
+				if inf.Kind() == reflect.Ptr {
+					if keyName == tag.Key {
+						relation := inf.Elem().FieldByName(tag.Value)
+						t.setValue(relation, of)
+						continue
+					}
+				}
+			}
+
+			if keyName == otf.Name {
+				if otf.Type.Name() == "Time" {
+					of.SetString(t.setTime(inf, "", timeFormat))
+					continue
+				}
+
+				if inf.Type() == of.Type() {
+					t.setValue(inf, of)
+					continue
+				}
+			}
+
+			//if keyName == "BaseModel" {
+			//	if otf.Name == "Id" {
+			//		of.SetInt(inf.FieldByName("Id").Interface().(int64))
+			//		continue
+			//	} else if otf.Name == "CreatedAt" {
+			//		of.SetString(t.setTime(inf, "CreatedAt", timeFormat))
+			//		continue
+			//	} else if otf.Name == "UpdatedAt" {
+			//		of.SetString(t.setTime(inf, "UpdatedAt", timeFormat))
+			//		continue
+			//	}
+			//}
+
+		}
+	}
 }
 
 // call out func
